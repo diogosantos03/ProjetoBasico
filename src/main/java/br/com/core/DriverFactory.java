7c55472d22cccd12e4da1d50ff6c40b592d20fdf
Diogo <diogo.santos@dce.ufpb.br>
2018-09-20 21:35:30 -0300

    Utilizando GRID

--- src/main/java/br/com/core/DriverFactory.java
+++ src/main/java/br/com/core/DriverFactory.java
@@ -1,8 +1,15 @@
 package br.com.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import br.com.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
 //	private static WebDriver driver;
@@ -22,16 +29,35 @@ public class DriverFactory {
 	}
 	public static WebDriver initDriver() {
 		WebDriver driver = null;
-		switch (Propriedades.browsers) {
-		case FIREFOX:
-			driver = new FirefoxDriver();
-			break;
-		case CHROME:
-			driver = new ChromeDriver();
-			break;
-			
-			//driver.manage().window().setPosition(new Point(100, 100));
-			//driver.manage().window().setSize(new Dimension(700, 700));
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
+			switch (Propriedades.BROWSERS) {
+			case FIREFOX:
+				driver = new FirefoxDriver();
+				break;
+			case CHROME:
+				driver = new ChromeDriver();
+				break;
+				
+				//driver.manage().window().setPosition(new Point(100, 100));
+				//driver.manage().window().setSize(new Dimension(700, 700));
+			}
+		}
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSERS) {
+			case FIREFOX:
+				cap = DesiredCapabilities.firefox();
+				break;
+			case CHROME:
+				cap = DesiredCapabilities.chrome();
+				break;
+			}
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.0.105:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.out.println("Falha na conexão com grid!");
+				e.printStackTrace();
+			}
 		}
 		return driver;
 	}
--- src/main/java/br/com/core/Propriedades.java
+++ src/main/java/br/com/core/Propriedades.java
@@ -3,12 +3,19 @@ package br.com.core;
 public class Propriedades {
 	public static boolean FECHAR_BROWSER = true;
 	
-	public static Browsers browsers = Browsers.FIREFOX;
+	public static Browsers BROWSERS = Browsers.FIREFOX;
 	
 	public static String NOME_CONTA_ALTERADA = "Conta Alterada "+ System.nanoTime();
+	
+	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
 	
 	public enum Browsers{
 		CHROME,
 		FIREFOX,
+	}
+	
+	public enum TipoExecucao{
+		LOCAL,
+		GRID
 	}
 }
